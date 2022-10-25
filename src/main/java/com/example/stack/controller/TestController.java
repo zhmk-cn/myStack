c5286196435f7e7fae076291631b6532dc60b75a
zhmk-cn <zmk199406155>
2022-10-25 16:10:16 +0800

    格式化

--- src/main/java/com/example/stack/controller/TestController.java
+++ src/main/java/com/example/stack/controller/TestController.java
@@ -1,4 +1,5 @@
 package com.example.stack.controller;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import sun.misc.BASE64Encoder;
+
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import java.awt.*;
@@ -23,25 +25,27 @@ import java.util.Objects;
 
 @Controller
 class TestController {
+
     @GetMapping("/")
-    public String uploladPage(){
+    public String uploladPage() {
         return "upload";
     }
+
     @PostMapping("/upload")
-    public String fileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest req, Model model){
+    public String fileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest req, Model model) {
         try {
             //接收上传文件
             //Receiving uploaded files
-            String fileName = System.currentTimeMillis()+file.getOriginalFilename();
-            String destFileName=req.getServletContext().getRealPath("")+"uploaded"+ File.separator+fileName;
+            String fileName = System.currentTimeMillis() + file.getOriginalFilename();
+            String destFileName = req.getServletContext().getRealPath("") + "uploaded" + File.separator + fileName;
             File destFile = new File(destFileName);
             destFile.getParentFile().mkdirs();
             System.out.println(destFile);
             file.transferTo(destFile);
             //向前端模板引擎传入上传文件的地址
             //The address of the uploaded file is passed in to the front-end template engine
-            model.addAttribute("fileName","uploaded\\"+fileName);
-            model.addAttribute("path",destFile);
+            model.addAttribute("fileName", "uploaded\\" + fileName);
+            model.addAttribute("path", destFile);
             //开始准备请求API
             //Start preparing the request API
             //创建请求头
@@ -52,7 +56,7 @@ class TestController {
             headers.setContentType(MediaType.APPLICATION_JSON);
             //构建请求参数
             //Build request parameters
-            MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+            MultiValueMap <String, String> map = new LinkedMultiValueMap <String, String>();
             //读入静态资源文件
             //Read the static resource file
             InputStream imagePath = new FileInputStream(destFile);
@@ -61,7 +65,7 @@ class TestController {
             map.add("images", ImageToBase64(imagePath));
             //构建请求
             //Build request
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+            HttpEntity <MultiValueMap <String, String>> request = new HttpEntity <MultiValueMap <String, String>>(map, headers);
             RestTemplate restTemplate = new RestTemplate();
             //发送请求
             //Send the request
@@ -69,7 +73,7 @@ class TestController {
             System.out.println(json);
             //解析Json返回值
             //Parse the Json return value
-            List<List<Map>> json1 = (List<List<Map>>) json.get("results");
+            List <List <Map>> json1 = (List <List <Map>>) json.get("results");
             //获取文件目录为后面画图做准备
             //Get the file directory to prepare for later drawing
             String tarImgPath = destFile.toString();
@@ -95,17 +99,17 @@ class TestController {
             for (int i = 0; i < json1.get(0).size(); i++) {
                 System.out.println("当前的文字是：" + json1.get(0).get(i).get("text"));
                 System.out.println("可能的概率为：" + json1.get(0).get(i).get("confidence"));
-                List<List<Integer>> json2 = (List<List<Integer>>) json1.get(0).get(i).get("text_region");
+                List <List <Integer>> json2 = (List <List <Integer>>) json1.get(0).get(i).get("text_region");
                 System.out.println("文字的坐标" + json2);
                 int x = json2.get(0).get(0);
                 int y = json2.get(0).get(1);
-                int w = json2.get(1).get(0)-json2.get(0).get(0);
-                int h = json2.get(2).get(1)-json2.get(0).get(1);
-                g.drawRect(x,y,w,h);  //画出水印   Draw the watermark
+                int w = json2.get(1).get(0) - json2.get(0).get(0);
+                int h = json2.get(2).get(1) - json2.get(0).get(1);
+                g.drawRect(x, y, w, h);  //画出水印   Draw the watermark
             }
             //将内容提交到前端模板引擎
             //Submit the content to the front-end template engine
-            model.addAttribute("z",json1.get(0));
+            model.addAttribute("z", json1.get(0));
             g.dispose();
             // 输出图片
             //The output image
@@ -123,6 +127,7 @@ class TestController {
         }
         return "OK";
     }
+
     private String ImageToBase64(InputStream imgPath) {
         byte[] data = null;
         // 读取图片字节数组
