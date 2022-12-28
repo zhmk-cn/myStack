5dfe4f7069bb6713fa6fa18139c708a77a77fabc
zhmk-cn <zmk199406155>
2022-12-28 10:57:55 +0800

    SchedulerLock 任务锁效果测试

--- src/main/java/com/example/stack/StackApplication.java
+++ src/main/java/com/example/stack/StackApplication.java
@@ -1,11 +1,13 @@
 package com.example.stack;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class StackApplication {
 
     public static void main(String[] args) {
